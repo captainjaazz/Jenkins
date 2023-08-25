2701e56659cb568d8b048af0bccd14a91d4b604a
jaazz <jasimjaazz97@gmail.com>
2023-08-25 07:39:08 +0530

    Added compile code

--- src/main/java/com/jaazz/devops/samples/main.java
+++ src/main/java/com/jaazz/devops/samples/main.java
@@ -1,5 +1,7 @@
+package main.java.com.jaazz.devops.samples;
+
 class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
